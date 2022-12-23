e9db5f4dc8f687b3a83364983873831494dd8b90
newt <newtykins@outlook.com>
2022-12-23 18:52:45 +0000

    feat(problem): 3 - largest prime factor

---
+++ src/bin/3.rs
@@ -0,0 +1,35 @@
+/*
+Problem 3 - Largest prime factor
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
+*/
+
+fn largest_prime_factor(number: usize) -> usize {
+	// All even numbers can be divided by 2
+	if number % 2 == 0 {
+		return 2;
+	}
+
+	let mut i = 3;
+	let mut num = number.clone();
+	let mut primes: Vec<usize> = vec![];
+
+	while num != 1 {
+		if num % i == 0 {
+			num /= i;
+			primes.push(i);
+		} else {
+			// Test next odd number
+			i += 2;
+		}
+	}
+
+	// The largest prime factor will be at the end of the vec
+	return primes[primes.len() - 1];
+}
+
+fn main() {
+	let factor = largest_prime_factor(600851475143);
+	println!("The largest prime factor of 600851475143 is {factor}");
+}

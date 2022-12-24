ebc51427cb8b1bfe2960a3f7a01fffd1e5b2ed0b
newt <newtykins@outlook.com>
2022-12-24 00:58:45 +0000

    feat(tools): readme updater!

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use clap::{Parser, Subcommand};
 use owo_colors::OwoColorize;
-use std::{fs, path::Path, io::Write};
+use std::{fs::{self, File, OpenOptions}, path::Path, io::{Write, BufRead}};
 use scraper::{Html, Selector};
 use regex::Regex;
+use std::io::{BufReader};
 
 #[derive(Parser)]
 #[clap(about, author, version)]
@@ -19,7 +20,8 @@ enum Commands {
 
 #[tokio::main]
 async fn new() -> Result<(), Box<dyn std::error::Error>> {
-    let code_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("src").join("bin");
+    let base_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+    let code_path = base_dir.join("src").join("bin");
 
     let problem_number = requestty::prompt_one(
         requestty::Question::int("problemNumber")
@@ -61,10 +63,10 @@ async fn new() -> Result<(), Box<dyn std::error::Error>> {
     // Fetch the problem information
     let body = reqwest::get(format!("https://projecteuler.net/problem={problem_number}")).await?.text().await?;
 
-    let document = Html::parse_document(Box::leak(body.into_boxed_str()));
+    let document = Html::parse_document(body.as_str());
     let title_selector = Selector::parse("h2")?;
     let content_selector = Selector::parse(".problem_content")?;
-    let re = Regex::new(r"<[^>]*>").unwrap();
+    let html_tag_regex = Regex::new(r"<[^>]*>").unwrap();
 
     let mut title = document
         .select(&title_selector)
@@ -74,16 +76,14 @@ async fn new() -> Result<(), Box<dyn std::error::Error>> {
         .collect::<Vec<&str>>()
         .join("");
 
-    let mut problem = re
+    let mut problem = html_tag_regex
         .replace_all(
-            Box::leak(
-                document
+            document
                     .select(&content_selector)
                     .next()
                     .unwrap()
                     .inner_html()
-                    .into_boxed_str()
-            ), 
+                    .as_str(),
             " "
         )
         .to_string()
@@ -110,11 +110,60 @@ fn main() {{
     );
 
     // Create the file
-    let mut file = fs::File::create(code_path.join(format!("{problem_number}.rs"))).unwrap();
+    let mut file = File::create(code_path.join(format!("{problem_number}.rs"))).unwrap();
     file.write(file_body.as_bytes()).unwrap();
     drop(file);
 
-    // todo: update readme
+    // Read the contents of the readme for editing
+    let readme_path = base_dir.join("readme.md");
+
+    let mut readme_file = OpenOptions::new()
+        .read(true)
+        .open(&readme_path)
+        .unwrap();
+
+    let mut readme_content = BufReader::new(&readme_file)
+        .lines()
+        .map(|s| s.unwrap())
+        .collect::<Vec<String>>();
+
+
+    drop(readme_file);
+
+    // Mark the problem as done on the readme
+    let readme_regex = Regex::new(format!(" {problem_number} - (.*)").as_str()).unwrap();
+
+    for i in 0..readme_content.len() {
+        let line = readme_content[i].as_str();
+
+        if readme_regex.is_match(line) {
+            let matched = readme_regex.captures(line).unwrap();
+            readme_content[i] = format!("- [x] {problem_number} - [{}](src/bin/{problem_number}.rs)", &matched[1].trim());
+        }
+    }
+
+    // Update the summary statistics on the readme
+    let mut readme_string = readme_content.join("\n");
+
+    let completed_regex = Regex::new("<!-- completed -->([0-9]+)<!-- completed -->").unwrap();
+
+    let new_completed = completed_regex
+        .captures(readme_string.as_str())
+        .unwrap()[1]
+        .parse::<u8>()
+        .unwrap() + 1;
+
+    readme_string = completed_regex.replace(readme_string.as_str(), format!("<!-- completed -->{new_completed}<!-- completed -->")).to_string();
+
+    // Write the new content to the readme
+    readme_file = OpenOptions::new()
+        .write(true)
+        .open(&readme_path)
+        .unwrap();
+
+    readme_file.write(readme_string.as_bytes()).unwrap();
+
+    drop(readme_file);
 
     // Announce completion!
     println!("{}", "File successfully created! Good luck (:".green());
